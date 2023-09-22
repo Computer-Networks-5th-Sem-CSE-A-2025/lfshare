ce769aa442caa3a2799512c179cdb5dc93835a9c
thrombe <yash.n.thombare620@gmail.com>
2023-09-23 02:05:41 +0530

    can transfer files now

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use std::fs::File;
 use std::io::{Read, Write};
 use std::net::{TcpListener, TcpStream};
 use std::{
@@ -23,41 +24,51 @@ enum Command {
     Send {
         #[arg(long, short)]
         port: u16,
+        #[arg(long, short)]
+        file: PathBuf,
     },
 
     Listen {
         #[arg(long, short)]
         port: u16,
+        #[arg(long, short)]
+        file: PathBuf,
     },
 }
 
 fn main() {
     let cli = Cli::parse();
     match cli.command {
-        Command::Send { port } => {
+        Command::Send { port, file } => {
             let mut stream =
                 TcpStream::connect("127.0.0.1:8080").expect("Failed to connect to server");
 
-            let message = "Hello, server!";
-            stream
-                .write_all(message.as_bytes())
-                .expect("Failed to send data");
+            let filename = file
+                .file_name()
+                .expect("Failed to extract filename")
+                .to_string_lossy()
+                .into_owned();
+
+            // Send the filename to the server
+            stream.write_all(filename.as_bytes()).expect("Failed to send filename");
+
+            let mut file = File::open(file).expect("Failed to open file");
 
             let mut buffer = [0; 512];
-            match stream.read(&mut buffer) {
-                Ok(bytes_read) => {
-                    println!(
-                        "Received {} bytes from server: {:?}",
-                        bytes_read,
-                        &buffer[..bytes_read]
-                    );
-                }
-                Err(e) => {
-                    println!("Failed to read data from server: {:?}", e);
+            loop {
+                match file.read(&mut buffer) {
+                    Ok(0) => break, // End of file
+                    Ok(n) => {
+                        stream.write_all(&buffer[..n]).expect("Failed to send data");
+                    }
+                    Err(e) => {
+                        println!("Failed to read data from file: {:?}", e);
+                        break;
+                    }
                 }
             }
         }
-        Command::Listen { port } => {
+        Command::Listen { port, file } => {
             let listener = TcpListener::bind("0.0.0.0:8080").expect("Failed to bind to address");
 
             println!("Server listening on 0.0.0.0:8080...");
@@ -70,16 +81,28 @@ fn main() {
                         let mut buffer = [0; 512];
                         match stream.read(&mut buffer) {
                             Ok(bytes_read) => {
-                                let s = String::from_utf8(buffer[..bytes_read].into()).unwrap();
-                                dbg!(s);
-                                println!(
-                                    "Received {} bytes: {:?}",
-                                    bytes_read,
-                                    &buffer[..bytes_read]
-                                );
-                                stream
-                                    .write_all(&buffer[..bytes_read])
-                                    .expect("Failed to write data");
+                                let filename = String::from_utf8_lossy(&buffer[..bytes_read]);
+                                let _ = fs::create_dir("./received_files");
+                                let file_path = Path::new("./received_files").join(filename.trim());
+                                dbg!(&file_path);
+
+                                let mut file = File::create(&file_path).expect("Failed to create file");
+
+                                // Receive and write the file contents
+                                loop {
+                                    match stream.read(&mut buffer) {
+                                        Ok(0) => break, // End of file
+                                        Ok(n) => {
+                                            file.write_all(&buffer[..n]).expect("Failed to write data");
+                                        }
+                                        Err(e) => {
+                                            println!("Failed to read data: {:?}", e);
+                                            break;
+                                        }
+                                    }
+                                }
+
+                                println!("Received file: {:?}", file_path);
                             }
                             Err(e) => {
                                 println!("Failed to read data: {:?}", e);
